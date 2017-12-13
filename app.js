e3eaed0580ef738560c7bb119225c805b0806f23
qiucheng <2697446878@qq.com>
2017-12-13 16:18:49 +0800

    project init

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+let path = require('path');
+
+//设定静态文件目录，比如本地文件
+app.use(express.static(path.join(__dirname, 'public')));
+
+//接口
+
+app.listen(3000, ()=>{
+    console.log('running at port 3000');
+});
+
